4125626b222cd74b8db3e9e9b1b74f7b4a0b25bb
emanKhalil <43522855+emanKhalil@users.noreply.github.com>
2019-04-30 02:33:51 +0300

    bean

---
+++ oss/src/java/Beans/CitizenRequestManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Eman
+ */
+@ManagedBean
+@ViewScoped
+public class CitizenRequestManager implements Serializable {
+    
+}
---
+++ oss/src/java/Data/CitizenRequest.java
@@ -0,0 +1,252 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author Eman
+ */
+public class CitizenRequest {
+    
+    int Cit_ID;
+    
+String Cit_FirstName;
+String Cit_FatherName; 
+String Cit_GrandfatherName; 
+String Cit_LastName;
+String Cit_Gender; 
+int Cit_FamilyMembers;
+String Cit_ID_Card; 
+String Cit_Telephone;
+String Cit_Mobile; 
+String Cit_Email;
+String Cit_Fax;
+String Cit_Birthday;
+String Cit_PlaceOfBirth;
+String Cit_Region;
+String Cit_Quarter; 
+String Cit_Street;
+String Cit_Address; 
+String Cit_Job;
+String Cit_PassportNumber; 
+String Cit_PassportType; 
+String Cit_Username; 
+String Cit_Password;
+
+    public CitizenRequest(int Cit_ID, String Cit_FirstName, String Cit_FatherName, String Cit_GrandfatherName, String Cit_LastName, String Cit_Gender, int Cit_FamilyMembers, String Cit_ID_Card, String Cit_Telephone, String Cit_Mobile, String Cit_Email, String Cit_Fax, String Cit_Birthday, String Cit_PlaceOfBirth, String Cit_Region, String Cit_Quarter, String Cit_Street, String Cit_Address, String Cit_Job, String Cit_PassportNumber, String Cit_PassportType, String Cit_Username, String Cit_Password) {
+        this.Cit_ID = Cit_ID;
+        this.Cit_FirstName = Cit_FirstName;
+        this.Cit_FatherName = Cit_FatherName;
+        this.Cit_GrandfatherName = Cit_GrandfatherName;
+        this.Cit_LastName = Cit_LastName;
+        this.Cit_Gender = Cit_Gender;
+        this.Cit_FamilyMembers = Cit_FamilyMembers;
+        this.Cit_ID_Card = Cit_ID_Card;
+        this.Cit_Telephone = Cit_Telephone;
+        this.Cit_Mobile = Cit_Mobile;
+        this.Cit_Email = Cit_Email;
+        this.Cit_Fax = Cit_Fax;
+        this.Cit_Birthday = Cit_Birthday;
+        this.Cit_PlaceOfBirth = Cit_PlaceOfBirth;
+        this.Cit_Region = Cit_Region;
+        this.Cit_Quarter = Cit_Quarter;
+        this.Cit_Street = Cit_Street;
+        this.Cit_Address = Cit_Address;
+        this.Cit_Job = Cit_Job;
+        this.Cit_PassportNumber = Cit_PassportNumber;
+        this.Cit_PassportType = Cit_PassportType;
+        this.Cit_Username = Cit_Username;
+        this.Cit_Password = Cit_Password;
+    }
+
+    public int getCit_ID() {
+        return Cit_ID;
+    }
+
+    public void setCit_ID(int Cit_ID) {
+        this.Cit_ID = Cit_ID;
+    }
+
+    public String getCit_FirstName() {
+        return Cit_FirstName;
+    }
+
+    public void setCit_FirstName(String Cit_FirstName) {
+        this.Cit_FirstName = Cit_FirstName;
+    }
+
+    public String getCit_FatherName() {
+        return Cit_FatherName;
+    }
+
+    public void setCit_FatherName(String Cit_FatherName) {
+        this.Cit_FatherName = Cit_FatherName;
+    }
+
+    public String getCit_GrandfatherName() {
+        return Cit_GrandfatherName;
+    }
+
+    public void setCit_GrandfatherName(String Cit_GrandfatherName) {
+        this.Cit_GrandfatherName = Cit_GrandfatherName;
+    }
+
+    public String getCit_LastName() {
+        return Cit_LastName;
+    }
+
+    public void setCit_LastName(String Cit_LastName) {
+        this.Cit_LastName = Cit_LastName;
+    }
+
+    public String getCit_Gender() {
+        return Cit_Gender;
+    }
+
+    public void setCit_Gender(String Cit_Gender) {
+        this.Cit_Gender = Cit_Gender;
+    }
+
+    public int getCit_FamilyMembers() {
+        return Cit_FamilyMembers;
+    }
+
+    public void setCit_FamilyMembers(int Cit_FamilyMembers) {
+        this.Cit_FamilyMembers = Cit_FamilyMembers;
+    }
+
+    public String getCit_ID_Card() {
+        return Cit_ID_Card;
+    }
+
+    public void setCit_ID_Card(String Cit_ID_Card) {
+        this.Cit_ID_Card = Cit_ID_Card;
+    }
+
+    public String getCit_Telephone() {
+        return Cit_Telephone;
+    }
+
+    public void setCit_Telephone(String Cit_Telephone) {
+        this.Cit_Telephone = Cit_Telephone;
+    }
+
+    public String getCit_Mobile() {
+        return Cit_Mobile;
+    }
+
+    public void setCit_Mobile(String Cit_Mobile) {
+        this.Cit_Mobile = Cit_Mobile;
+    }
+
+    public String getCit_Email() {
+        return Cit_Email;
+    }
+
+    public void setCit_Email(String Cit_Email) {
+        this.Cit_Email = Cit_Email;
+    }
+
+    public String getCit_Fax() {
+        return Cit_Fax;
+    }
+
+    public void setCit_Fax(String Cit_Fax) {
+        this.Cit_Fax = Cit_Fax;
+    }
+
+    public String getCit_Birthday() {
+        return Cit_Birthday;
+    }
+
+    public void setCit_Birthday(String Cit_Birthday) {
+        this.Cit_Birthday = Cit_Birthday;
+    }
+
+    public String getCit_PlaceOfBirth() {
+        return Cit_PlaceOfBirth;
+    }
+
+    public void setCit_PlaceOfBirth(String Cit_PlaceOfBirth) {
+        this.Cit_PlaceOfBirth = Cit_PlaceOfBirth;
+    }
+
+    public String getCit_Region() {
+        return Cit_Region;
+    }
+
+    public void setCit_Region(String Cit_Region) {
+        this.Cit_Region = Cit_Region;
+    }
+
+    public String getCit_Quarter() {
+        return Cit_Quarter;
+    }
+
+    public void setCit_Quarter(String Cit_Quarter) {
+        this.Cit_Quarter = Cit_Quarter;
+    }
+
+    public String getCit_Street() {
+        return Cit_Street;
+    }
+
+    public void setCit_Street(String Cit_Street) {
+        this.Cit_Street = Cit_Street;
+    }
+
+    public String getCit_Address() {
+        return Cit_Address;
+    }
+
+    public void setCit_Address(String Cit_Address) {
+        this.Cit_Address = Cit_Address;
+    }
+
+    public String getCit_Job() {
+        return Cit_Job;
+    }
+
+    public void setCit_Job(String Cit_Job) {
+        this.Cit_Job = Cit_Job;
+    }
+
+    public String getCit_PassportNumber() {
+        return Cit_PassportNumber;
+    }
+
+    public void setCit_PassportNumber(String Cit_PassportNumber) {
+        this.Cit_PassportNumber = Cit_PassportNumber;
+    }
+
+    public String getCit_PassportType() {
+        return Cit_PassportType;
+    }
+
+    public void setCit_PassportType(String Cit_PassportType) {
+        this.Cit_PassportType = Cit_PassportType;
+    }
+
+    public String getCit_Username() {
+        return Cit_Username;
+    }
+
+    public void setCit_Username(String Cit_Username) {
+        this.Cit_Username = Cit_Username;
+    }
+
+    public String getCit_Password() {
+        return Cit_Password;
+    }
+
+    public void setCit_Password(String Cit_Password) {
+        this.Cit_Password = Cit_Password;
+    }
+
+
+
+
+}
