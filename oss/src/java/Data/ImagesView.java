03e15c01644f30802f471ebbacb5461cd6ae5956
emanKhalil <43522855+emanKhalil@users.noreply.github.com>
2019-04-29 17:08:33 +0300

    done

--- oss/src/java/Data/ImagesView.java
+++ oss/src/java/Data/ImagesView.java
@@ -10,25 +10,43 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 /**
  *
  * @author Eman
  */
 @ManagedBean
+
 public class ImagesView implements Serializable{
      
     private List<String> images;
      
+    public ImagesView(){
+    }
+    
     @PostConstruct
     public void init() {
         images = new ArrayList<String>();
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 1; i <= 6; i++) {
             images.add("image" + i + ".jpg");
+            System.out.println(images);
         }
     }
- 
+
+    
+    public ImagesView(List<String> images) {
+        this.images = images;
+    }
+    
+   
     public List<String> getImages() {
         return images;
     }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+ 
+    
 }
