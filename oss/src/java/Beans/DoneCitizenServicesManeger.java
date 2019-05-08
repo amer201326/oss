59a588574812efc480b0a90c4ec61637807628bc
BaraaKali <47575983+BaraaKali@users.noreply.github.com>
2019-05-08 23:33:33 +0300

    f

--- oss/src/java/Beans/DoneCitizenServicesManeger.java
+++ oss/src/java/Beans/DoneCitizenServicesManeger.java
@@ -6,14 +6,21 @@
 package Beans;
 
 import Data.CitizenService;
+import Data.Department;
+import Data.GetFromDB;
 import Data.GetFromDBaraa;
+import Data.ServiceCitizen;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -22,24 +29,47 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean
 @ViewScoped
 public class DoneCitizenServicesManeger implements Serializable {
-
+    List<CitizenService> allDoneCitizenServices ;
     List<CitizenService> doneCitizenServices ;
     @ManagedProperty(value = "#{msession}")
     Session session;
     int idCitizen;
-
+    int idDepartment;
+    
         @PostConstruct
  public void init() {
      if(session.citizen != null){
             idCitizen = session.citizen.getId();
-         doneCitizenServices = GetFromDBaraa.doneCitizenServices(idCitizen);
+         allDoneCitizenServices = GetFromDBaraa.doneCitizenServices(idCitizen);
+         doneCitizenServices = new ArrayList<>();
+         for (CitizenService allDoneCitizenService : allDoneCitizenServices) {
+             doneCitizenServices.add(allDoneCitizenService);
+         }
         }
  }
     public DoneCitizenServicesManeger() {
         
        
     }
-
+    public void filter() {
+       
+      doneCitizenServices.clear();
+      if(idDepartment == -1){
+           for (CitizenService allDoneCitizenService : allDoneCitizenServices) {
+             doneCitizenServices.add(allDoneCitizenService);
+         }
+      }else{
+        for (CitizenService doneCitizenService : allDoneCitizenServices) {
+            if(doneCitizenService.getProvidServicee().getDepartment().id == idDepartment){
+                doneCitizenServices.add(doneCitizenService);
+            }
+        }
+      }
+    }
+     public List<Department> allDepartment() {
+        return GetFromDB.getDepartments();
+    }
+     
     public boolean dontHaveService(){
         return doneCitizenServices.isEmpty();
     }
@@ -63,4 +93,30 @@ public class DoneCitizenServicesManeger implements Serializable {
     public boolean nothaveSer(){
         return doneCitizenServices.isEmpty();
     }
+
+    public int getIdDepartment() {
+        return idDepartment;
+    }
+
+    public void setIdDepartment(int idDepartment) {
+        this.idDepartment = idDepartment;
+    }
+   public void showServiceCitizen(int idServiceCitizen) {
+        
+        try {
+             for (ServiceCitizen service : allDoneCitizenServices) {
+            if (service.getService_Citizen_ID() == idServiceCitizen) {
+                session.serviceCitizenShow = service;
+            }
+        }
+         
+             FacesContext.getCurrentInstance().getExternalContext().redirect("ShowMyService.xhtml");
+
+        } catch (IOException ex) {
+            Logger.getLogger(NotDoneCiticenServiceManeger.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+       
+    } 
 }
